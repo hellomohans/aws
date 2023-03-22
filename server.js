954429b99cc854bf397a474c55adde11b516c44b
Mohan <97966135+Enterweb123@users.noreply.github.com>
2023-03-22 12:01:43 +0530

    firest commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("<h1> Welcome to awa class </h1>");
+})
+app.get("/new", (req,res)=>{
+    res.send("New route is working");
+});
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("app is working");
+});
+
+module.exports = app;
